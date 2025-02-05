399999cda404cacb3e84c5471feabc3fdfbf07b1
boumahdi <boumahdi@live.fr>
2025-02-05 10:46:36 +0000

    Mise à jour du bot pour python-telegram-bot v20

--- bot.py
+++ bot.py
@@ -1,29 +1,23 @@
 import webbrowser
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, CallbackContext
 
-# Ouvre automatiquement index.html dans le navigateur
-webbrowser.open(f'file://{os.path.abspath("index.html")}')
+# Remplacez par le token de votre bot
+TOKEN = "7963739930:AAGLR3reZEHliOWOG6lshDZ02miBzkSOAdg"
 
-# Fonction de démarrage pour la commande /start
-async def start(update: Update, context) -> None:
-    await update.message.reply_text('Bienvenue au Puzzle Game! Jouez ici: https://boumahdi25.github.io/PuzzleGame')
+async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text("Bonjour! Je suis votre bot.")
 
-# Fonction principale qui lance le bot
-def main() -> None:
-    # Remplacez 'VOTRE_TOKEN_D_ACCES' par le token de votre bot Telegram
-    token = "7963739930:AAGLR3reZEHliOWOG6lshDZ02miBzkSOAdg"
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
 
-
-    # Création de l'application avec le token
-    app = ApplicationBuilder().token(token).build()
-
-    # Ajouter un gestionnaire de commande pour la commande /start
+    # Commande /start
     app.add_handler(CommandHandler("start", start))
 
-    # Démarrer le bot
+    # Lancer le bot
     app.run_polling()
 
 if __name__ == '__main__':
     main()
+
