b4c4c2e9e1e4c8cf93f0c3627cf468076e865e54
boumahdi25 <boumahdi@live.fr>
2025-02-14 13:39:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 from flask import Flask, request
 import os
-from telegram import Bot, Update
-from telegram.ext import CommandHandler, CallbackContext, Updater
+from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, CallbackQueryHandler, ApplicationBuilder
 
 app = Flask(__name__)
 
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 bot = Bot(token=TOKEN)
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Hello! Welcome to the bot.")
+def start(update: Update, context):
+    keyboard = [
+        [InlineKeyboardButton("Bouton 1", callback_data='1')],
+        [InlineKeyboardButton("Bouton 2", callback_data='2')],
+        [InlineKeyboardButton("Bouton 3", callback_data='3')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Choisissez une option:', reply_markup=reply_markup)
+
+def button(update: Update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=f"Vous avez cliqué sur le bouton {query.data}")
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     update = Update.de_json(request.get_json(force=True), bot)
-    dispatcher.process_update(update)
+    application.update_queue.put(update)
     return "ok"
 
 if __name__ == "__main__":
-    updater = Updater(token=TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(os.environ.get('PORT', 5000)),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://aljazeera.onrender.com/' + TOKEN)
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button))
+
+    application.run_webhook(
+        listen="0.0.0.0",
+        port=int(os.environ.get('PORT', 5000)),
+        url_path=TOKEN,
+        webhook_url='https://boumahdi25.github.io/AlJazeera/' + TOKEN
+    )
+
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
